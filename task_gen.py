ccba904badb6ee62256c7995e1694def81cdb1e4
Kenny Ning <kennyning@gmail.com>
2018-06-24 19:05:48 -0400

    add 2nd mind map

--- task_gen.py
+++ task_gen.py
@@ -1,18 +1,20 @@
 from mind_map import data
+import json
 import random
 
-def pretty_print(category, subcategory, project):
+def pretty_print(category, subcategory, project=None):
   line_break = "#----------------------------------------------------#"
 
   print(line_break)
   print("CATEGORY")
   print(category)
-
+  print()
   print("SUBCATEGORY OF THE WEEK")
-  print(sub_category)
+  print(subcategory)
 
-  print("==> %s" % project['project_name'])
-  print("==> %s" % project['description'])
+  if project:
+    print("==> %s" % project['project_name'])
+    print("==> %s" % project['description'])
 
   print(line_break)
 
@@ -21,9 +23,7 @@ def log_progress():
   Write the results to a spreadsheet after each run for easy logging
   """
 
-if __name__ == "__main__":
-  #TODO: cron this to run every Sunday
-
+def draw_mind_map():
   #should be 4 categories: Music, Data Science, Social, and Physical
   cat_draw = random.randint(0, 3)
 
@@ -40,8 +40,27 @@ if __name__ == "__main__":
   projects = sub_cat_dict.get(sub_category)
   proj_draw = random.randint(0, len(projects)-1)
   project = projects[proj_draw]
-  
-  pretty_print(category, sub_category, project)
+
+def draw_mind_map2():
+  MIND_MAP2_PATH = "mind_map2.json"
+
+  with open(MIND_MAP2_PATH, "r") as f:
+    data = json.loads(f.read())
+
+  #draw random category
+  category_draw = random.randint(0,2)
+  category = list(data.keys())[category_draw]
+
+  #draw random subcategory
+  sc_draw = random.randint(0, len(data[category])-1)
+  sub_category = data[category][sc_draw]
+
+  return category, sub_category
+
+if __name__ == "__main__":
+  #TODO: cron this to run every Sunday
+  a,b = draw_mind_map2()
+  pretty_print(a, b)
 
   print("log progress at: https://docs.google.com/spreadsheets/d/1P7gnigZ_36vtrPwM5niAd4cJl-d0_PIJBcOlqdPkbu4/edit#gid=1501612727")
 
